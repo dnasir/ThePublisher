980182b1fd5eacb8f651bbb4d35ff81a5a99ad6f
Dzulqarnain Nasir <dzul1983@gmail.com>
2019-04-23 17:45:19 +0300

    Moved config to local.settings.json.

--- DailyBuilder/index.ts
+++ DailyBuilder/index.ts
@@ -1,17 +1,18 @@
 import { AzureFunction, Context } from '@azure/functions';
 import axios, { AxiosError } from 'axios';
-import config from 'root/package.json';
 
 const timerTrigger: AzureFunction = async function (context: Context): Promise<void> {
-  if (!config.github) {
+  const { GithubUsername, GithubRepo, GithubToken } = process.env;
+
+  if (!GithubUsername || !GithubRepo || !GithubToken) {
     context.log.error('GitHub settings missing. Exiting.');
     return context.done();
   }
 
   try {
-    const response = await axios.post(`https://api.github.com/repos/${config.github.username}/${config.github.repo}/pages/builds`, null, {
+    const response = await axios.post(`https://api.github.com/repos/${GithubUsername}/${GithubRepo}/pages/builds`, null, {
       headers: {
-        'Authorization': `token ${config.github.token}`,
+        'Authorization': `token ${GithubToken}`,
         'Accept': 'application/vnd.github.mister-fantastic-preview+json',
         'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'
       }
