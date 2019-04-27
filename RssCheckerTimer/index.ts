2bbbf68f0e5b1ff785ce2d5876892a99fa6184be
Dzulqarnain Nasir <dzul1983@gmail.com>
2019-04-27 19:18:13 +0300

    Added null check for RssCheckerTimer function.

--- RssCheckerTimer/index.ts
+++ RssCheckerTimer/index.ts
@@ -3,6 +3,11 @@ import { AzureFunction, Context } from '@azure/functions';
 const func: AzureFunction = async function (context: Context): Promise<void> {
   const { RssFeedUrl } = process.env;
 
+  if (!RssFeedUrl) {
+    context.log('[RssCheckerTimer] No feed URL found. Exiting.');
+    return context.done();
+  }
+
   context.log(`[RssCheckerTimer] Processing RSS feed URL: ${RssFeedUrl}.`);
 
   context.bindings.outputQueueItem = RssFeedUrl;
